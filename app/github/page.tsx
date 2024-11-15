30fbd09b49fc29969b985f67466b408d15d70e03
Jbassard <jonathanacciarito@gmail.com>
2024-11-14 23:18:26 -0500

    creating a little stats bar to show how many repos I have, how many total contributions, and later on: the longest streak I've had

--- app/github/page.tsx
+++ app/github/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 
 import Link from "next/link";
-import GitHubCalendar from "../components/GitHubCalendar";
+import GitHubCalendar from "../components/GitHubCalendar/GitHubCalendar";
+import "./github-page.css";
 
 const GitHub: React.FC = () => {
   return (
-    <div>
-      <h1>My GitHub Contributions</h1>
-      <GitHubCalendar />
+    <div className="github-page">
+      <h1 className="underline">JBassard97's GitHub</h1>
+      <div className="github-container">
+        <GitHubCalendar />
+      </div>
     </div>
   );
 };
