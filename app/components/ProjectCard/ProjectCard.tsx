099fb394fe68ef3c68d03522206c4e5b11d4480d
Jbassard <jonathanacciarito@gmail.com>
2024-12-03 12:42:03 -0500

    really fudging with the layout of the ProjectCard component. Trying to find a happy medium of responsiveness and aesthetics

--- app/components/ProjectCard/ProjectCard.tsx
+++ app/components/ProjectCard/ProjectCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { Tooltip } from "react-tooltip";
 import { Project } from "../../interfaces";
 import techToLogo from "../../techToLogo";
 // import StackDisplay from "../StackDisplay/StackDisplay";
@@ -12,60 +13,94 @@ interface ProjectCardProps {
 
 // Component to be used in /projects/all AND /projects/that_use/[technology]
 const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
+  const projectTypeToIcon = (type: string) => {
+    switch (type) {
+      case "Web App":
+        return "/globe.svg";
+      case "API":
+        return "/api.svg";
+      case "Terminal App":
+        return "/powershell.svg";
+      case "Other":
+        return "/spiral.svg";
+      default:
+        return "/charizard.png";
+    }
+  };
+
   return (
     <div className="project">
-      <div className="project-image">
-        {/* <Image
-          src="/charizard.png"
-          alt={project.name}
-          width="500"
-          height="300"
-          layout="responsive"
-        /> */}
+      <Image
+        className="project-type-icon"
+        src={projectTypeToIcon(project.type)}
+        alt={project.type}
+        data-tooltip-id={project.type}
+        data-tooltip-content={project.type}
+        width="25"
+        height="25"
+      />
+      <Tooltip id={project.type} />
+
+      <div className="project-image-container">
+        <img src="/charizard.png"></img>
       </div>
       <div className="project-data">
         <div className="project-text">
           <Link href={`/projects/${project.name}`}>
-            <p>{project.name}</p>
+            <p className="project-name">{project.name}</p>
           </Link>
-          <p>{project.desc}</p>
+          <p className="project-desc">{project.desc}</p>
         </div>
-        <div className="stack-type-display">
-          {project.hasBackend && <p className="backend">Back End</p>}
-          {project.hasFrontend && <p className="frontend">Front End</p>}
-          {project.isFullStack && <p className="fullstack">Full Stack</p>}
-          {!project.hasBackend &&
-            !project.hasFrontend &&
-            !project.isFullStack && <p className="other">Other</p>}
-        </div>
-        <div className="project-links">
-          {/* {project.isComplete ? <p>Complete</p> : <p>In Progress</p>} */}
-          {project.isDeployed && project.deployedLink.length > 0 && (
-            <Link href={project.deployedLink}>
-              <div className="deployed-link">
-                <p>View</p>
+
+        <div className="all-buttons">
+          <div className="stack-type-display">
+            {project.hasBackend && <p className="backend">Back End</p>}
+            {project.hasFrontend && <p className="frontend">Front End</p>}
+            {project.isFullStack && <p className="fullstack">Full Stack</p>}
+            {!project.hasBackend &&
+              !project.hasFrontend &&
+              !project.isFullStack && <p className="other">Other</p>}
+          </div>
+
+          <div className="stack-display">
+            {project.stack.map((technology, index) => (
+              <Link href={`/projects/that_use/${technology}`} key={index}>
                 <Image
-                  src="/charizard.png"
-                  alt={`${project.name} Link`}
-                  width="25"
-                  height="25"
+                  className="technology-image"
+                  src={techToLogo(technology)}
+                  alt={technology}
+                  height="30"
+                  width="30"
+                  data-tooltip-id={technology}
+                  data-tooltip-content={technology}
                 />
+                <Tooltip id={technology} />
+              </Link>
+            ))}
+          </div>
+
+          <div className="project-links">
+            {/* {project.isComplete ? <p>Complete</p> : <p>In Progress</p>} */}
+            {project.isDeployed && project.deployedLink.length > 0 && (
+              <Link href={project.deployedLink}>
+                <div className="deployed-link">
+                  <p>Deployed</p>
+                  <Image
+                    src={techToLogo(project.hostedOn)}
+                    alt="Deployed"
+                    width="25"
+                    height="25"
+                  />
+                </div>
+              </Link>
+            )}
+            <Link href={project.githubLink}>
+              <div className="github-link">
+                <p>View</p>
+                <Image src="/github.svg" alt="Github" width="25" height="25" />
               </div>
             </Link>
-          )}
-        </div>
-        {/* <StackDisplay stack={project.stack} /> */}
-        <div className="stack-display">
-          {project.stack.map((technology, index) => (
-            <Image
-              className="technology-image"
-              key={index}
-              src={techToLogo(technology)}
-              alt={technology}
-              height="25"
-              width="25"
-            />
-          ))}
+          </div>
         </div>
       </div>
     </div>
